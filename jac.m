3949ef570e11347a39d40553fff08d805afde28b
Mohammad Hossein Shojaeinia <mhs.shojaeinia@gmail.com>
2017-06-19 17:24:04 +0430

    Jacobian iterative method for equation solving

---
+++ jac.m
@@ -0,0 +1,22 @@
+function jac(coefs, vars, ress, iters)
+    for k = 1:iters
+        temp = zeros(size(vars));
+        
+        if((size(coefs, 2) ~= size(vars, 1)) && (size(ress, 2) ~= size(vars, 1)))
+            disp('check marices sizes');
+            return;
+        else 
+           for i = 1:(size(coefs, 1))
+                temp(i, 1) = ress(i, 1);
+                for j = 1:(size(coefs, 2))
+                    if(i ~= j)
+                        temp(i, 1) = (temp(i, 1)) - (coefs(i, j)*vars(j, 1));
+                    end
+                end
+                temp(i, 1) = temp(i, 1) / coefs(i, i);
+           end
+        end
+        vars = temp;
+    end
+    temp
+end
